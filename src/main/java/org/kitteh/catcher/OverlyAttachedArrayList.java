678c5d95d9acdcdea48b3d7f436c484e3dc8ab29
mbax <matt@phozop.net>
2012-11-09 01:49:17 -0500

    Implement proper logging, also blame.

--- src/main/java/org/kitteh/catcher/OverlyAttachedArrayList.java
+++ src/main/java/org/kitteh/catcher/OverlyAttachedArrayList.java
@@ -6,186 +6,150 @@ import java.util.*;
 public class OverlyAttachedArrayList extends ArrayList {
     private static final long serialVersionUID = 4671186665144729041L;
     private final Thread thread;
+    private final Plugin plugin;
 
-    public OverlyAttachedArrayList(ArrayList list) {
+    public OverlyAttachedArrayList(Plugin plugin, ArrayList list) {
         super(list);
+        this.plugin = plugin;
         this.thread = Thread.currentThread();
     }
 
     @Override
     public void add(int index, Object element) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         super.add(index, element);
     }
 
     @Override
     public boolean add(Object e) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.add(e);
     }
 
     @Override
     public boolean addAll(Collection c) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.addAll(c);
     }
 
     @Override
     public boolean addAll(int index, Collection c) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.addAll(index, c);
     }
 
     @Override
     public void clear() {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         super.clear();
     }
 
     @Override
     public boolean contains(Object o) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.contains(o);
     }
 
     @Override
     public boolean containsAll(Collection c) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.containsAll(c);
     }
 
     @Override
     public Object get(int index) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.get(index);
     }
 
     @Override
     public int indexOf(Object o) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.indexOf(o);
     }
 
     @Override
     public boolean isEmpty() {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.isEmpty();
     }
 
     @Override
     public Iterator<Object> iterator() {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.iterator();
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.lastIndexOf(o);
     }
 
     @Override
     public ListIterator<Object> listIterator() {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.listIterator();
     }
 
     @Override
     public ListIterator<Object> listIterator(int index) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.listIterator(index);
     }
 
     @Override
     public Object remove(int index) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.remove(index);
     }
 
     @Override
     public boolean remove(Object o) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.remove(o);
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.removeAll(c);
     }
 
     @Override
     public boolean retainAll(Collection c) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.retainAll(c);
     }
 
     @Override
     public Object set(int index, Object element) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.set(index, element);
     }
 
     @Override
     public int size() {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.size();
     }
 
     @Override
     public List<Object> subList(int fromIndex, int toIndex) {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.subList(fromIndex, toIndex);
     }
 
     @Override
     public Object[] toArray() {
-        if (!Thread.currentThread().equals(this.thread)) {
-            new CaughtInTheAct(new Throwable()).printStackTrace();
-        }
+        this.check();
         return super.toArray();
+    }
+
+    private void check() {
+        if (!Thread.currentThread().equals(this.thread)) {
+            this.plugin.list.add(new Throwable().fillInStackTrace());
+        }
     }
 
 }
--- src/main/java/org/kitteh/catcher/Plugin.java
+++ src/main/java/org/kitteh/catcher/Plugin.java
@@ -1,15 +1,119 @@
 package org.kitteh.catcher;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.*;
 
 import net.minecraft.server.World;
 
 import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+import org.bukkit.plugin.java.PluginClassLoader;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class Plugin extends JavaPlugin {
+
+    private class Frmttr extends java.util.logging.Formatter {
+        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        @Override
+        public String format(LogRecord record) {
+            final StringBuilder builder = new StringBuilder();
+            builder.append(this.date.format(record.getMillis()));
+            builder.append(" [");
+            builder.append(record.getLevel().getLocalizedName().toUpperCase());
+            builder.append("] ");
+            builder.append(this.formatMessage(record));
+            builder.append('\n');
+            final Throwable ex = record.getThrown();
+            if (ex != null) {
+                ex.setStackTrace(Arrays.copyOfRange(ex.getStackTrace(), 1, ex.getStackTrace().length));
+                final StringWriter writer = new StringWriter();
+                ex.printStackTrace(new PrintWriter(writer));
+                builder.append(writer);
+            }
+
+            return builder.toString();
+        }
+    }
+
+    private class Output implements Runnable {
+
+        @Override
+        public void run() {
+            Map<String, PluginClassLoader> loaders;
+            try {
+                loaders = (Map<String, PluginClassLoader>) Plugin.this.jplLoaders.get(Plugin.this.getPluginLoader());
+            } catch (final IllegalArgumentException e) {
+                e.printStackTrace();
+                return;
+            } catch (final IllegalAccessException e) {
+                e.printStackTrace();
+                return;
+            }
+            boolean found = false;
+            while (!Plugin.this.list.isEmpty()) {
+                found = true;
+                final Throwable t = Plugin.this.list.remove(0);
+                final String thisName = Plugin.this.getDescription().getName();
+                final Set<String> set = new HashSet<String>();
+                for (final String plugin : loaders.keySet()) {
+                    if (plugin.equals(thisName)) {
+                        continue;
+                    }
+                    final PluginClassLoader loader = loaders.get(plugin);
+                    Map<String, Class<?>> classes;
+                    try {
+                        classes = (Map<String, Class<?>>) Plugin.this.pclClasses.get(loader);
+                    } catch (final IllegalArgumentException e2) {
+                        e2.printStackTrace();
+                        return;
+                    } catch (final IllegalAccessException e2) {
+                        e2.printStackTrace();
+                        return;
+                    }
+                    for (final StackTraceElement e : t.getStackTrace()) {
+                        if (classes.containsKey(e.getClassName())) {
+                            set.add(plugin);
+                        }
+                    }
+                }
+                String message;
+                if (set.isEmpty()) {
+                    message = "Found an async call I can't trace";
+                } else {
+                    final StringBuilder builder = new StringBuilder();
+                    builder.append("Found async call. Might be from: ");
+                    for (final String plugin : set) {
+                        final org.bukkit.plugin.Plugin p = Plugin.this.getServer().getPluginManager().getPlugin(plugin);
+                        builder.append(plugin);
+                        builder.append(" (");
+                        for (final String author : p.getDescription().getAuthors()) {
+                            builder.append(author).append(" ");
+                        }
+                        builder.append(") ");
+                    }
+                    message = builder.toString();
+                }
+                Plugin.this.logger.log(Level.WARNING, message, t);
+            }
+            if (found) {
+                Plugin.this.getLogger().severe("Found an async call. Check " + Plugin.this.asyncLogFile);
+            }
+        }
+
+    }
+
+    private Field jplLoaders;
+    private Field pclClasses;
+    private Logger logger;
+    private File asyncLogFile;
+    public List<Throwable> list = Collections.synchronizedList(new ArrayList<Throwable>());
 
     @Override
     public void onDisable() {
@@ -39,7 +143,23 @@ public class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.logger = Logger.getLogger("PluginCatcher");
+        this.getDataFolder().mkdir();
+        this.asyncLogFile = new File(this.getDataFolder(), "async.log");
         try {
+            final FileHandler handler = new FileHandler(this.asyncLogFile.getAbsolutePath(), true);
+            handler.setFormatter(new Frmttr());
+            this.logger.addHandler(handler);
+            this.logger.setUseParentHandlers(false);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            this.logger = this.getLogger();
+        }
+        try {
+            this.jplLoaders = JavaPluginLoader.class.getDeclaredField("loaders");
+            this.jplLoaders.setAccessible(true);
+            this.pclClasses = PluginClassLoader.class.getDeclaredField("classes");
+            this.pclClasses.setAccessible(true);
             final Field fieldEntities = World.class.getDeclaredField("entityList");
             fieldEntities.setAccessible(true);
             final Field fieldPlayers = World.class.getDeclaredField("players");
@@ -48,9 +168,9 @@ public class Plugin extends JavaPlugin {
             fieldTileEntities.setAccessible(true);
             for (final org.bukkit.World bworld : this.getServer().getWorlds()) {
                 final World world = ((CraftWorld) bworld).getHandle();
-                fieldEntities.set(world, new OverlyAttachedArrayList((ArrayList) fieldEntities.get(world)));
-                fieldPlayers.set(world, new OverlyAttachedArrayList((ArrayList) fieldPlayers.get(world)));
-                fieldTileEntities.set(world, new OverlyAttachedArrayList((ArrayList) fieldTileEntities.get(world)));
+                fieldEntities.set(world, new OverlyAttachedArrayList(this, (ArrayList) fieldEntities.get(world)));
+                fieldPlayers.set(world, new OverlyAttachedArrayList(this, (ArrayList) fieldPlayers.get(world)));
+                fieldTileEntities.set(world, new OverlyAttachedArrayList(this, (ArrayList) fieldTileEntities.get(world)));
             }
         } catch (final SecurityException e) {
             e.printStackTrace();
@@ -61,6 +181,7 @@ public class Plugin extends JavaPlugin {
         } catch (final IllegalAccessException e) {
             e.printStackTrace();
         }
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Output(), 20, 20);
     }
 
 }
