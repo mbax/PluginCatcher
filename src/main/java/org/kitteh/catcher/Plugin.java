b510066e567dbec29ba901ca200af895cfc4a38e
mbax <matt@phozop.net>
2013-03-16 17:17:32 -0400

    User friendliness
    Added LICENSE. This project is now licensed under Apache 2.0
    Now checks supported version on startup
    Config.yml documentation

--- src/main/java/org/kitteh/catcher/Plugin.java
+++ src/main/java/org/kitteh/catcher/Plugin.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Matt Baxter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kitteh.catcher;
 
 import java.io.File;
@@ -134,6 +149,7 @@ public class Plugin extends JavaPlugin {
         }
     }
 
+    private boolean failed = false;
     private String thisName;
     private Field jplLoaders;
     private Field pclClasses;
@@ -156,6 +172,9 @@ public class Plugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        if (this.failed) {
+            return;
+        }
         try {
             final Field fieldEntities = World.class.getDeclaredField("entityList");
             fieldEntities.setAccessible(true);
@@ -182,6 +201,15 @@ public class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        final String packageName = this.getServer().getClass().getPackage().getName();
+        final String version = packageName.substring(packageName.lastIndexOf('.') + 1);
+        final String supports = "${minecraft_version}";
+        if (!version.equals(supports)) {
+            this.getLogger().severe("Incompatible versions. Supports " + supports + ", found " + version);
+            this.failed = true;
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
         final File file = new File(this.getDataFolder(), "config.yml");
         if (!file.exists()) {
             this.saveDefaultConfig();
